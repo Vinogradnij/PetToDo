f4e89b8f71d0c46885369f515fb01224733fbdec
Vinogradnij <vlud5@mail.ru>
2022-09-23 16:48:07 +0300

    Создание проекта

---
+++ mainpage/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+
+# Create your models here.
+
+class Todo(models.Model):
+    name = models.CharField(max_length=30)
+    text = models.CharField(max_length=255)
+    time_create = models.DateTimeField(auto_now_add=True)
