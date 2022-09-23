317306c7b09af5ebdb909005f5c35bf90db47569
Vinogradnij <vlud5@mail.ru>
2022-09-23 19:15:23 +0300

    Вывод на страницу списка задач из БД

--- mainpage/models.py
+++ mainpage/models.py
@@ -7,3 +7,6 @@ class Todo(models.Model):
     name = models.CharField(max_length=30)
     text = models.CharField(max_length=255)
     time_create = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
--- mainpage/views.py
+++ mainpage/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
+from mainpage.models import Todo
 
 
 # Create your views here.
 
 def index(request):
-    return render(request, 'mainpage/index.html')
+    title = 'Все задачи'
+    tasks = Todo.objects.all()
+    return render(request, 'mainpage/index.html', {'tasks': tasks, 'title': title})
