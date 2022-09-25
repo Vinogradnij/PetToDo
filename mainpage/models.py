4d4d20036d3d8591c41a604e507151fe7730a02b
Vinogradnij <vlud5@mail.ru>
2022-09-25 23:46:58 +0300

    Добавлена первичная модель Category, изменена вторичная модель Todo

--- mainpage/models.py
+++ mainpage/models.py
@@ -7,6 +7,14 @@ class Todo(models.Model):
     name = models.CharField(max_length=30)
     text = models.CharField(max_length=255)
     time_create = models.DateTimeField(auto_now_add=True)
+    category = models.ForeignKey('Category', on_delete=models.PROTECT, null=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=30, db_index=True)
 
     def __str__(self):
         return self.name
